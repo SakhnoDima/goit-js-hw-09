cbf8cc48fe7e30da8c8937da107c1acf8a84d649
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-20 13:52:34 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs ={
+    btnStart : document.querySelector("[data-start]"),
+    btnStop : document.querySelector("[data-stop]"),
+    body : document.body,
+}
+let TIMER_ID = null;
+let disabled = false;
 
+refs.btnStart.addEventListener("click", onBtnStartClick);
+refs.btnStop.addEventListener("click", onBtnStopClick);
+
+
+function onBtnStartClick (event){
+if (!disabled) {
+    TIMER_ID = setInterval((changeColor), 1000);
+
+    refs.btnStart.disabled = true;
+}
+};
+
+function onBtnStopClick(event){
+    refs.btnStart.disabled = false;
+    clearInterval(TIMER_ID);  
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+function changeColor() {
+    refs.body.style.backgroundColor = `${getRandomHexColor()}`
+};
