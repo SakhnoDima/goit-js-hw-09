2435ff3fbc976b870afa67db0cfc26b5c8d36aed
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-20 16:55:46 +0300

    add flatpickr

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    inputForDate :document.querySelector("#datetime-picker")
+} 
+console.log(refs.inputForDate);
