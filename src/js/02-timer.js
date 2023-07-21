35804b0d6c9852dba7bfc8ba7a36de62f1fc0ce9
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-21 13:46:03 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,12 +2,15 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const refs = {
-    inputForDate :document.querySelector("#datetime-picker"),
+    inputForDate : document.querySelector("#datetime-picker"),
     btnStart : document.querySelector("[data-start]"),
+    days : document.querySelector("[data-days]"),
+    hours : document.querySelector("[data-hours]"),
+    minutes : document.querySelector("[data-minutes]"),
+    seconds : document.querySelector("[data-seconds]"),
 } 
-
-
 refs.btnStart.setAttribute("disabled", true)
+
 
 const fp = flatpickr(refs.inputForDate, {
     enableTime: true,
@@ -17,35 +20,31 @@ const fp = flatpickr(refs.inputForDate, {
     onClose(selectedDates) {
 
 refs.btnStart.addEventListener("click", onBtnClick)
-const selectedDate = selectedDates[0].getTime();
-const dateNow = new Date().getTime();
-const timeToCount = selectedDate - dateNow;
+const selectedDate = selectedDates[0].getTime(); // выбранное время
+const timeToCount = selectedDate - Date.now(); // выбранное - текущее время 
+
 if ( timeToCount< 0 ){
     alert("Please choose a date in the future");
 } else if (timeToCount> 0 ){
 refs.btnStart.removeAttribute("disabled");
 }
 
-
-
-
-
-
-
-
 function onBtnClick (event) {
-  const time = convertMs(timeToCount)
+ const intervalId = setInterval(() => {
+    const time = (Date.now() - selectedDate)* (-1) 
     console.log(time);
-    console.log("x");
-    refs.btnStart.setAttribute("disabled", true)
+  if (time < 0){
+    alert("Timer is finish");
+    clearInterval(intervalId);
+    return
+  } 
+    const timeS= convertMs(time)
+    const timeWithZero = addLeadingZero(timeS);
+    updateTime(timeWithZero);
+  }, 1000)
+
+  refs.btnStart.setAttribute("disabled", true)
 }
-
-      
-
-     
-
- 
-      
 
     },
   });
@@ -68,3 +67,17 @@ function onBtnClick (event) {
   
     return { days, hours, minutes, seconds };
   }
+  function addLeadingZero(value){
+    const { days, hours, minutes, seconds } = value;
+    const DAYS = String(days).padStart(2, "0");
+    const HOURS =  String(hours).padStart(2, "0");
+    const MINUTES =  String(minutes).padStart(2, "0");
+    const SECONDS = String(seconds).padStart(2, "0");
+     return { DAYS, HOURS, MINUTES, SECONDS }
+  }
+  function updateTime ({ DAYS, HOURS, MINUTES, SECONDS }) {
+    refs.days.textContent = `${DAYS}`;
+    refs.hours.textContent = `${HOURS}`;
+    refs.minutes.textContent = `${MINUTES}`;
+    refs.seconds.textContent = `${SECONDS}`;
+  } 
